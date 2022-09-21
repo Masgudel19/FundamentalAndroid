b95888e1e5edb3fe81e09d17c942f378e5056b74
AkhmadAldi19 <aldiakhmad15@gmail.com>
2022-09-21 23:03:13 +0700

    Mengirim Data pada Intent

--- Fundamental/MyIntentApp/app/src/main/java/com/aldiakhmad19/myintentapp/MainActivity.kt
+++ Fundamental/MyIntentApp/app/src/main/java/com/aldiakhmad19/myintentapp/MainActivity.kt
@@ -13,6 +13,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveActivity:Button = findViewById(R.id.btn_move_activity)
         btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveWithDataActivity:Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveWithDataActivity.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -24,6 +27,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
                 // startActivity(moveIntent) akan menjalankan activity baru tanpa membawa data.
                 startActivity(moveIntent)
+            }
+
+            R.id.btn_move_activity_data ->{
+                val moveWithDataIntent = Intent(this@MainActivity,MoveWithDataActivity::class.java)
+                // metode putExtra() untuk mengirimkan data bersamaan dengan obyek Intent
+                // metode putExtra() sendiri merupakan metode yang menampung pasangan key-value
+                // EXTRA_NAME adalah variabel static bertipe data string dan bernilai extra_name pada MoveWithDataActivity.
+                // DicodingAcademy Gudel dengan tipe data string.
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Gudel")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE,21)
+                startActivity((moveWithDataIntent))
+
             }
         }
     }
